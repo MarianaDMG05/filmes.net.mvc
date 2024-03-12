08b8e5328bcb1f3c6d6cd9cff2a695254b8167a4
MarianaDMG05 <mariana.gomes87@etec.sp.gov.br>
2024-03-12 20:39:18 -0300

    linha 84

---
+++ MeusFilmes/Models/Série.cs
@@ -0,0 +1,11 @@
+
+namespace MeusFilmes.Models
+{
+    public class Série
+    {
+     
+     public int Nome{ get; set; } 
+     public int Gênero{ get; set; }  
+     
+     public int DFescrição { get; set; }      }
+}
